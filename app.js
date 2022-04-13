0c8022d414575933880e71a2c7e62d1f485c576c
“PrinceXxShadows” <princexxshadows@gmail.com>
2022-04-13 09:31:48 -0500

    complete

---
+++ app.js
@@ -0,0 +1,46 @@
+setTimeout(() => {
+    const div1 = document.querySelector(`#first`);
+    const p1 = document.createElement(`p`);
+    p1.innerText = `Hi`;
+    div1.append(p1);
+}, 1000);
+
+setTimeout(() => {
+    const div2 = document.querySelector(`#timeout-nesting`);
+    const p2 = document.createElement(`p`);
+    p2.innerText = `One`;
+    div2.append(p2);
+    setTimeout(() => {
+        const p3 = document.createElement(`p`);
+        p3.innerText = `Two`;
+        div2.append(p3);
+    }, 1000);      
+}, 2000);
+
+let second = 1;
+const time = setInterval(() => {
+    console.log(`${second}`);
+    second++;
+}, 1000);
+
+const stop = document.querySelector(`button`);
+stop.addEventListener(`click`, () => {
+    clearInterval(time);
+});
+
+const div3 = document.querySelector(`#countdown`);
+const p3 = document.createElement(`p`);
+let num = 120;
+const count = setInterval(() => {
+    if (num > 0){
+        let minutes = Math.floor(num/60);
+        let seconds = num - minutes * 60;
+        p3.innerText = `${minutes}:${seconds}`;
+        div3.append(p3);
+    } else {
+        clearInterval(count);
+        p3.innerText =`TIME IS UP`;
+        div3.append(p3);
+    }
+    num--;
+}, 1000);
